2bfb45720cb06fc8c5ef83a02ad973daf2bcdb7c
Anatoliy Ostrovskiy <megatolya@yandex-team.ru>
2013-04-06 17:36:17 +0400

    delete method

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/yadisk.js');
+module.exports = require('./lib/core.js');
--- test.js
+++ test.js
@@ -10,3 +10,14 @@ api._request('POST', '/test?publish').then(function() {
 }).fail(function() {
     console.log('fail', arguments);
 });
+
+api.delete('test.pdf')
+    .then(function(res) {
+        console.log('deleted');
+        console.log(res);
+    })
+    .fail(function(err) {
+        console.log('failed');
+        console.log(err);
+    })
+
